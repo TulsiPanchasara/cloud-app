4007dae49cb80e1b2ab6b0b96b26140725bffcc7
Harsh <harsh.mauny@actonate.com>
2022-04-21 16:27:34 +0530

    Added basic setup for Jest and react-testing-library

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testEnvironment: 'jsdom',
+}
